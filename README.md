2976035bdb0b4768d3f79d4bc54d6855f3fb01ab
Nasvad <davifreireangelim@gmail.com>
2022-04-12 15:10:28 -0300

    correção proposital pra criar o staging

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pejot
+# Trabalho de Engenharia de Software II
+
+## Aprendendo a configurar ambiente e usar branches
+
+### Davi Freire
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-eco "Praticando configuração de ambiente e staging";
+echo "Praticando configuração de ambiente e staging";
 ?>
 
 <iframe width="450" height="275" src="https://www.youtube.com/embed/eUM1lu3bMwk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
