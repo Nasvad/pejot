c0a1c598a0e00c5ca49080dec6e377a2a459a956
Nasvad <davifreireangelim@gmail.com>
2022-04-12 15:08:13 -0300

    trabai

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+eco "Praticando configuração de ambiente e staging";
+?>
+
+<iframe width="450" height="275" src="https://www.youtube.com/embed/eUM1lu3bMwk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <?php
+echo ";)";
+?>
+
+</body>
+</html>
